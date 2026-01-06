a0c40d0be59bcc6a0af6db1860b55d4769e83843
sylabtv <sylab.studio@gmail.com>
2026-01-06 15:57:55 +0100

    exercice 2 v2

--- exercice-boucles-conditions/exercice-2/index.js
+++ exercice-boucles-conditions/exercice-2/index.js
@@ -1,8 +1,10 @@
 let i = 1;
-while(i>=10){
-    console.log(i);
-    i++;
-    if(i % 2 === 0){
-        console.log("pair")
+while (i <= 10) {
+    if(i % 2 === 0) {
+            console.log(i + " pair");
     }
+    else {
+        console.log(i);
+    }
+    i++
 }
