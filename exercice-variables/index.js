ed4279e143252c0fa2fd1f9d65d56317dec2d52f
sylabtv <sylab.studio@gmail.com>
2026-01-06 14:05:45 +0100

    exercice variables js

---
+++ exercice-variables/index.js
@@ -0,0 +1,20 @@
+const character = "Legolas";
+console.log(character);
+
+let nom = "Legolas";
+console.log(nom);
+
+nom = "gimli";
+console.log(nom);
+
+let x = 1000;
+console.log(x)
+
+let y = 1.5;
+console.log(y)
+
+let z = -500;
+console.log(z)
+
+let truth = true;
+let lie = false;
